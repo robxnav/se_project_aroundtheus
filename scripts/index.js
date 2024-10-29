29f6a349d987d3aa6a65d71caa538d0e2e086c47
robxnav <robnavarro729@gmail.com>
2024-10-28 21:45:26 -0400

    Replaced card HTML with JS template

--- scripts/index.js
+++ scripts/index.js
@@ -33,11 +33,48 @@ const initialCards = [
 const editBtn = document.querySelector(".profile__edit-button");
 const editModal = document.querySelector(".modal");
 const closeModal = document.querySelector(".modal__close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const modalTitleInput = document.querySelector("#modal-title-input");
+const modalSubtitleInput = document.querySelector("#modal-subtitle-input");
+const modalEditForm = editModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+function closePopup() {
+  editModal.classList.remove("modal_opened");
+}
+
+function handleModalEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = modalTitleInput.value;
+  profileSubtitle.textContent = modalSubtitleInput.value;
+  closePopup();
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__caption");
+  const cardImage = cardElement.querySelector(".card__image");
+  console.log(cardData);
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  return cardElement;
+}
 
 editBtn.addEventListener("click", () => {
+  modalTitleInput.value = profileTitle.textContent;
+  modalSubtitleInput.value = profileSubtitle.textContent;
   editModal.classList.add("modal_opened");
 });
 
-closeModal.addEventListener("click", () => {
-  editModal.classList.remove("modal_opened");
+closeModal.addEventListener("click", closePopup);
+
+modalEditForm.addEventListener("submit", handleModalEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.prepend(cardElement);
 });
